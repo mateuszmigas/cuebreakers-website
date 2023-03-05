d23fd1d08b5e5bba0dfc682edbb5d8dea524be23
Mateusz Migas <mateusz.migas@innergy.com>
2023-03-05 10:50:04 +0100

    fixed build

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,7 +27,7 @@ export const Section2 = () => {
       <div className="relative h-[400px] w-[800px] p-[40px]">
         <div className="relative p-5 pl-[400px]">
           Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          industry. Lorem Ipsum has been the industrys standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
           scrambled it to make a type specimen book. It has survived not only
           five centuries, but also the leap into electronic typesetting,
